db2c45028d6474ab8b76cdfe2f1c53e925105a28
Thet Naing Tun <tnttun@gmail.com>
2025-09-15 19:14:22 +0800

    Simplify the Route web file

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,16 @@
 <?php
 
-use App\Livewire\Settings\Appearance;
-use App\Livewire\Settings\Password;
-use App\Livewire\Settings\Profile;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
-
-Route::get('articles', [\App\Http\Controllers\ArticleController::class, 'index']);
-Route::get('articles/{id}', [\App\Http\Controllers\ArticleController::class, 'show']);
-Route::get('authors', [\App\Http\Controllers\AuthorController::class, 'index']);
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::get('articles', [\App\Http\Controllers\ArticleController::class, 'index']);
+Route::get('articles/{id}', [\App\Http\Controllers\ArticleController::class, 'show']);
 
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
-});
+Route::get('authors', [AuthorController::class, 'index']);
+Route::get('authors/{id}', [AuthorController::class, 'show']);
 
 require __DIR__.'/auth.php';
