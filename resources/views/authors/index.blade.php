88a8be7b912b4a37973c99663727ecc0a2743f55
Thet Naing Tun <tnttun@gmail.com>
2025-09-15 21:09:43 +0800

    Fix 🐛 Redesign the list of authors

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -1,10 +1,10 @@
 <x-site-layout>
-    <main class="max-w-6xl mx-auto px-4 py-4"
-          <h2 class="text-2xl font-bold mb-4">Authors</h2>
+    <main>
+    <h2 class="text-4xl font-bold mb-4">Authors</h2>
     <ul>
         @foreach($authors as $author)
             <li>
-                <a href="{{ route('authors.show', $author) }}" class="text-blue-600 hover:underline">
+                <a href="{{ route('authors.show', $author) }}" class="text-xl text-blue-600 hover:underline">
                 {{$author->name}}
                 </a>
             </li>
