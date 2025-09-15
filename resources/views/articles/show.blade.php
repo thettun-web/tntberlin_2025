d384544d782695e4d7def4b182defdbc153c8238
Thet Naing Tun <tnttun@gmail.com>
2025-09-15 20:20:51 +0800

    Added comments to Show articles

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -5,5 +5,13 @@
             <div>
                 {{$article->content}}
             </div>
+        <h2 class="text-2xl font-bold mt-4">Comments</h2>
+        <div>
+            @foreach($article->comments as $comment)
+                <div>
+                    {{$comment->content}}
+                </div>
+            @endforeach
+        </div>
     </main>
 </x-site-layout>
