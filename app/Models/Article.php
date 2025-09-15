939013d9e91ac95f2eb336a19f732eb16093e017
Thet Naing Tun <tnttun@gmail.com>
2025-09-15 20:16:39 +0800

    Added relation to comment and article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -12,5 +12,9 @@ class Article extends Model
     {
         return $this->belongsTo(User::class,'author_id');
     }
-    //
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'article_id');
+    }//
 }
