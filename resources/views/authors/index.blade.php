710347c43668438f6c2fc5c7f820d8d6a6b88e8c
Thet Naing Tun <tnttun@gmail.com>
2025-09-15 20:45:42 +0800

    Feature ❇️ Added the hyperlinks to the authors list

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -1,8 +1,12 @@
 <x-site-layout>
+    <main class="max-w-6xl mx-auto px-4 py-4"
+          <h2 class="text-2xl font-bold mb-4">Authors</h2>
     <ul class="list-disc pl-4">
         @foreach($authors as $author)
             <li>
+                <a href="{{ route('authors.show', $author) }}" class="text-blue-600 hover:underline">
                 {{$author->name}}
+                </a>
             </li>
         @endforeach
     </ul>
