f4f0de4df23629f2200abb00a594e2b2c3ca83e4
Thet Naing Tun <tnttun@gmail.com>
2025-09-15 20:54:46 +0800

    Refactor 🔨 Removed the bullet points for Authors

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -1,7 +1,7 @@
 <x-site-layout>
     <main class="max-w-6xl mx-auto px-4 py-4"
           <h2 class="text-2xl font-bold mb-4">Authors</h2>
-    <ul class="list-disc pl-4">
+    <ul>
         @foreach($authors as $author)
             <li>
                 <a href="{{ route('authors.show', $author) }}" class="text-blue-600 hover:underline">
