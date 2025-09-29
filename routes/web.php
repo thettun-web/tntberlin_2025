9bd8d76412dd55a481b7bda07d1e3e3fe505fb92
Thet Naing Tun <tnttun@gmail.com>
2025-09-29 21:30:57 +0800

    Bug 🐛 Fixed the incorrect order of the create route

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,19 @@ Route::get('/',[\App\Http\Controllers\WelcomeController::class, 'index'])->name(
 
 // Routes anyone can see
 Route::get('articles', [\App\Http\Controllers\ArticleController::class, 'index'])->name('articles.index');
-Route::get('articles/{id}', [\App\Http\Controllers\ArticleController::class, 'show'])->name('articles.show');
 Route::get('authors', [AuthorController::class, 'index'])->name('authors.index');
 Route::get('authors/{id}', [AuthorController::class, 'show'])->name('authors.show');
+
+// This specific "create" route is now outside the group but still protected by auth middleware.
+// It MUST be defined before the general "show" route
+Route::get('articles/create', [\App\Http\Controllers\ArticleController::class, 'create'])->name('articles.create')->middleware('auth');
+
+// This route MUST come after the specific "create" route.
+Route::get('articles/{id}', [\App\Http\Controllers\ArticleController::class, 'show'])->name('articles.show');
 
 // Routes only for logged-in users
 Route::middleware('auth')->group(function () {
     Route::post('articles', [\App\Http\Controllers\ArticleController::class, 'store'])->name('articles.store');
-    Route::get('articles/create', [\App\Http\Controllers\ArticleController::class, 'create'])->name('articles.create');
     Route::get('articles/{id}/edit', [\App\Http\Controllers\ArticleController::class, 'edit'])->name('articles.edit');
     Route::put('articles/{id}', [\App\Http\Controllers\ArticleController::class, 'update'])->name('articles.update');
     Route::delete('articles/{id}', [\App\Http\Controllers\ArticleController::class, 'destroy'])->name('articles.destroy');
