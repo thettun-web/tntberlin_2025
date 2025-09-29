9ccbf4c40890f118ceece8db6493372d2c73f87a
Thet Naing Tun <tnttun@gmail.com>
2025-09-29 23:38:48 +0800

    Refactor 🔨 Added relationship between author id in comment table

--- database/migrations/2025_09_05_104007_create_comments_table.php
+++ database/migrations/2025_09_05_104007_create_comments_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->text('content');
-            $table->string('author');
-            $table->foreignId('article_id');
+            $table->foreignId('author_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('article_id')->constrained('articles')->onDelete('cascade');
             $table->timestamps();
         });
     }
