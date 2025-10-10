84d8a751f0f1360993ebcdbcdeb076bc34e81e80
Thet Naing Tun <tnttun@gmail.com>
2025-10-11 00:42:12 +0800

    Feature 🎨 Hyperlink the list of articles by users

--- resources/views/authors/show.blade.php
+++ resources/views/authors/show.blade.php
@@ -3,9 +3,14 @@
         <h1 class="text-4xl font-bold">{{$author->name}}</h1>
 
         <h2 class="text-2xl font-bold">List of articles</h2>
-
-        @foreach($author->articles as $article)
-            {{ $article->title }}<br/>
-        @endforeach
+        <ul class="space-y-2">
+            @foreach($author->articles as $article)
+                <li>
+                    <a href="{{ route('articles.show', $article) }}" class="text-lg text-blue-600 hover:underline">
+                        {{ $article->title }}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
     </main>
 </x-site-layout>
