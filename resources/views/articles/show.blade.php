12ceee50a16679bd01d26205f4b0bf8fc3deac2e
Thet Naing Tun <tnttun@gmail.com>
2025-10-15 02:34:59 +0800

    Feature ❇️ Show the Success message for the comment

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,5 +1,13 @@
 <x-site-layout>
     <main class="max-w-6xl mx-auto px-4 py-4">
+
+{{--        Feedback the success message if there is.--}}
+        @if (session('success'))
+            <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+                <span class="block sm:inline">{{ session('success') }}</span>
+            </div>
+        @endif
+
         <h1 class="text-4xl font-bold">{{$article->title}}</h1>
 
             <div>
